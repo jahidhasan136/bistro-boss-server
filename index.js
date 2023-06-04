84510e4300e61f1635847be57b10a2e8b1cab5f3
Jahid Hasan <jh3495689@gmail.com>
2023-06-04 20:23:55 +0600

    users collection

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 
 console.log(process.env.DB_USER)
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rxkguw5.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,21 +29,47 @@ async function run() {
         await client.connect();
 
 
+        const usersCollection = client.db("bistroDB").collection("users")
         const bistroCollection = client.db("bistroDB").collection("menu")
         const reviewsCollection = client.db("bistroDB").collection("reviews")
         const cartCollection = client.db("bistroDB").collection("carts")
 
+
+
+        //user collection api
+
+        app.get('/users', async (req, res)=>{
+            const result = await usersCollection.find().toArray()
+            res.send(result)
+        })
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = {email: user.email}
+            const existingUser = usersCollection.findOne(query)
+            if(existingUser){
+                res.send({message: 'user already exists'})
+            }
+            const result = await usersCollection.insertOne(user)
+            res.send(result)
+        })
+
+        //menu related apis
         app.get('/menu', async (req, res) => {
             const result = await bistroCollection.find().toArray()
             res.send(result)
         })
 
+
+        //reviews api
         app.get('/reviews', async (req, res) => {
             const result = await reviewsCollection.find().toArray()
             res.send(result)
         })
 
         
+
+        //cart related apis
         app.get('/carts', async (req, res) => {
             const email = req.query.email;
             console.log(email)
@@ -58,6 +84,13 @@ async function run() {
         app.post('/carts', async (req, res) => {
             const item = req.body;
             const result = await cartCollection.insertOne(item)
+            res.send(result)
+        })
+
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id 
+            const query = {_id: new ObjectId(id)}
+            const result = await cartCollection.deleteOne(query)
             res.send(result)
         })
 
